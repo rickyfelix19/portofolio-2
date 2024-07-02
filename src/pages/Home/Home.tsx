8ce4edc39b32f7429e6c8c27e59661e1d3312ebf
Ricky Felix <rfel4230@uni.sydney.edu.au>
2024-07-02 16:12:22 +0700

    created basic homepage

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import LinearGradient from "../../components/magicui/linear-gradient";
+import Bento from "../../components/Bento";
+
+function Home() {
+	return (
+		<div>
+			<div>
+				<LinearGradient />
+			</div>
+			<div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+				<h4 className="text-slate-800 text-xl sm:text-xl lg:text-xl tracking-tight text-center">
+					INTRODUCTION
+				</h4>
+
+				<h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
+					Transforming Concepts into Seamless User Experience
+				</h1>
+
+				<p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto">
+					Hi, I am Ricky Felix, a UI/UX Designer from Indonesia
+				</p>
+
+				<div className="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
+					<Bento />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
