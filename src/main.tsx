a1839a4dcd0d8b2165f598c5ea58c7d09936be17
Ricky Felix <rfel4230@uni.sydney.edu.au>
2024-07-25 13:56:14 +0700

    add react router and 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// react router
+// import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <div>404 Not Found</div>,
+	},
+	// {
+	//   path: '',
+	//   element:
+	// }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function 404() {
+  return (
+      <div >
+          404 Not Found
+          <Link to="/">Home</Link>
+    </div>
+  )
+}
