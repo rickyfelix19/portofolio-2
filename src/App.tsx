e2de032c053a78950f11a34248d1360dc9d108ce
Ricky Felix <rfel4230@uni.sydney.edu.au>
2024-08-07 18:12:15 +0700

    add projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+
 import Home from "./pages/Home";
 import NotFound404 from "./pages/NotFound404";
+import KIPI2022 from "./components/Projects/KIPI2022";
+import IDEAStudio from "./components/Projects/IDEAStudio";
+import MobileComp from "./components/Projects/MobileComp";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -8,7 +12,11 @@ function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
-			<Route path="/projects/:id" element={<p>test</p>} />
+			{/* <Route path="/projects/" element={<Home />} /> */}
+			{/* projects route */}
+			<Route path="/projects/kipi" element={<KIPI2022 />} />
+			<Route path="/projects/mobile-computing" element={<MobileComp />} />
+			<Route path="/projects/idea-studio" element={<IDEAStudio />} />
 			{/* Redirect to 404 */}
 			<Route path="*" element={<NotFound404 />} />
 		</Routes>
---
+++ src/components/Projects/KIPI2022.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "../Common/NavBar";
+import Footer from "../Common/Footer";
+
+function KIPI2022() {
+	return (
+		<>
+			<NavBar />
+			<div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32 text-white">
+				Hello World
+			</div>
+			<Footer/>
+		</>
+	);
+}
+
+export default KIPI2022;
